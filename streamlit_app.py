183dcde37a5971b4b96c82f88dc0fdb1bdf179c1
gowthamic86 <87257542+gowthamic86@users.noreply.github.com>
2022-10-08 18:37:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
-import streamlit
-streamlit.title('My parents eat healthy dinner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+import pandas
+
+fruit_listed = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(fruit_listed)
