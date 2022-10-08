e3411caec7b8d40e840de0587631814e2d2dce1a
gowthamic86 <87257542+gowthamic86@users.noreply.github.com>
2022-10-08 20:57:11 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+import pandas
+
+streamlit.title('My parents food')
+streamlit.header(':grapes: Breakfast Menu')
+streamlit.text(':watermelon: Omega 3 & Blueberry Oatmeal')
+streamlit.text(':blueberries: Kale, Spinach & Rocket Smoothie')
+streamlit.text(':baguette_bread: Hard-Boiled Free-Range Egg')
