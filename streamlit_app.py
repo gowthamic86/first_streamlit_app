bd8e0e7c270b93f3962d196e36ab3232ad8e7239
gowthamic86 <87257542+gowthamic86@users.noreply.github.com>
2022-10-08 20:53:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import pandas
 fruit_listed = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.dataframe(fruit_listed)
+streamlit.header(':grapes: Breakfast Menu')
+streamlit.text(':watermelon: Omega 3 & Blueberry Oatmeal')
+streamlit.text(':blueberries: Kale, Spinach & Rocket Smoothie')
+streamlit.text(':baguette_bread: Hard-Boiled Free-Range Egg')
