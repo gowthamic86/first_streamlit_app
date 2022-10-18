bb0c07a84735f1cb9bba7f112afeaed5cdbc3a61
gowthamic86 <87257542+gowthamic86@users.noreply.github.com>
2022-10-18 17:57:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
 streamlit.title('My parents healthy dinner')
+
+import snowflake.connector
+import pandas
+
+fruit_listed = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(fruit_listed)
