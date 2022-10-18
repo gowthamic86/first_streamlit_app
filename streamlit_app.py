0df1a1cc4ccdbc7b0e5e57c7efd2b8edb2eab626
gowthamic86 <87257542+gowthamic86@users.noreply.github.com>
2022-10-18 18:31:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,8 @@
 import streamlit
 
 streamlit.title('My parents healthy dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale ,Spinach and Rocket Smoothie')
+streamlit.text('Hard Boiled Free Range Egg')
 
-import snowflake.connector
-import pandas
-
-fruit_listed = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
-streamlit.dataframe(fruit_listed)
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
