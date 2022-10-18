b647c484a2db8925d4d5c676635cc2fd5312b601
gowthamic86 <87257542+gowthamic86@users.noreply.github.com>
2022-10-18 17:50:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,1 @@
-import snowflake.connector
-import pandas
 
-fruit_listed = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
-streamlit.dataframe(fruit_listed)
