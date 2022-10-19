8739c92b9651e8bdef656895e09ffc99de77df7c
gowthamic86 <87257542+gowthamic86@users.noreply.github.com>
2022-10-19 12:00:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,9 +11,6 @@ streamlit.text('Kale ,Spinach and Rocket Smoothie')
 streamlit.text('Hard Boiled Free Range Egg')
 
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
-my_fruit_list = my_fruit_list.set_index('Fruit')
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
