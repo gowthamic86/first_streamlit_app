aea9d680e777946e0ea7918c2132c1da6e8f5799
gowthamic86 <87257542+gowthamic86@users.noreply.github.com>
2022-10-19 15:45:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,3 +18,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 #Let's put a pick list here so they can pick the fruit they want to include
 fruit_selected = streamlit.multiselect("Pick some fruits", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruit_selected]
+
+add_my_fruit = streamlit.multiselect("add some fruits", list(my_fruit_list.index))
+
+streamlit.write('Thanks for adding), add_my_fruit)
+my_cur.execute("insert into fruit_load_list values('from streamlit')")                
