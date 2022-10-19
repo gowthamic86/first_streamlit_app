c2027454f22dabe318752fe2971f0786b3279b89
gowthamic86 <87257542+gowthamic86@users.noreply.github.com>
2022-10-19 16:00:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,3 @@
 import streamlit
-import pandas
-import requests
-import snowflake.connector
-from urllib.error import URLError
 
 streamlit.title('My parents healthy dinner')
-streamlit.header('Breakfast favourites')
-streamlit.text('omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale ,Spinach and Rocket Smoothie')
-streamlit.text('Hard Boiled Free Range Egg')
-streamlit.text('Avacado toast')
-streamlit.header('Build your own fruit smoothie')
-
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-
-#Let's put a pick list here so they can pick the fruit they want to include
-fruit_selected = streamlit.multiselect("Pick some fruits", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruit_selected]
-
-add_my_fruit = streamlit.multiselect("add some fruits", list(my_fruit_list.index))
-
-streamlit.write('Thanks for adding', add_my_fruit)
-my_cur.execute("insert into fruit_load_list values('from streamlit')")
