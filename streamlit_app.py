c24f2b7e7c153c0dfa715fdb4bc18bbae7179b71
gowthamic86 <87257542+gowthamic86@users.noreply.github.com>
2022-10-19 17:08:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,18 +25,25 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
 
-#New section to display fruityvice api respose
-streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+#New section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+       streamlit.error("Please select a fruit to get information.")
+  else:
+      fruityvice_reponse = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruitvice_normalized  = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruitvice_normalized)
+      
+ except URLError as e:
+     streamlit.error()
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-#take the json version of the response and normalise it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output it the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+#Allow the end user to add a fruit to yje list 
+def insert_row_snowflake(new_fruit):
+    with my_cnx.xursor() as my_cur:
+         my_cnx.execute("insert into fruit_load_list values('from streamlit')")
+         return "Thanks for adding" +new_fruit
 
 #dont run anything past here while we troubleshoot
 streamlit.stop()
