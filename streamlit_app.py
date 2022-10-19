4b7b3a7ce45b3d810dbadb5ec22a5f06e872467d
gowthamic86 <87257542+gowthamic86@users.noreply.github.com>
2022-10-19 19:04:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ def great_fruityvice_data(this_fruit_choice):
     return fruitvice_normalized
       
 #New section to display fruityvice api response
-streamlit.header("Fruityvice Fruit Advice')
+streamlit.header('Fruityvice Fruit Advice')
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
