eeac519cfc474dd769ea294942d6e56ed87700c6
gowthamic86 <87257542+gowthamic86@users.noreply.github.com>
2022-10-19 12:05:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,9 +12,9 @@ streamlit.text('Hard Boiled Free Range Egg')
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+my_cur.execute("SELECT * from fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit_load list contains")
+streamlit.dataframe(my_data_rows)
 
 
