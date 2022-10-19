5a870ed42ec0f4a70be6e6b04844b7e34ab7cf06
gowthamic86 <87257542+gowthamic86@users.noreply.github.com>
2022-10-19 12:22:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,13 @@ import pandas
 import requests
 import snowflake.connector
 
-
 streamlit.title('My parents healthy dinner')
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast favourites')
 streamlit.text('omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale ,Spinach and Rocket Smoothie')
 streamlit.text('Hard Boiled Free Range Egg')
+streamlit.text('Avacado toast')
+streamlit.header('Build your own fruit smoothie')
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -17,4 +18,6 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit_load list contains")
 streamlit.dataframe(my_data_rows)
 
+streamlit.write('Thanks for adding', add_my_fruit)
+my_cur.execute("insert into fruit_load_list values ('from streamlit')");
 
